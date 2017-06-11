0edbbf191faa7e5e779cb97fcdacbe14e357b5da
Ultrafeel <miketut@gmail.com>
2017-06-11 16:58:33 +0300

    timer 1.

--- timer.c
+++ timer.c
@@ -6,6 +6,7 @@
 #include <linux/version.h>
 
 #include <linux/timer.h>
+//#include <signal.h>
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
@@ -14,20 +15,43 @@ MODULE_AUTHOR("MML");
 MODULE_DESCRIPTION("A Simple kernel_hello_irq module");
 
 //static int calc_result = 0;
-static unsigned int prints_count = 4;
+typedef unsigned int uint;
+static uint  prints_count = 4;
 
-module_param(prints_count, unsigned int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+struct timer_list my_timer;
+module_param(prints_count, uint, (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH));
 MODULE_PARM_DESC(prints_count, "An integer , prints_count");
 
+volatile int num_of_prints = 0;
+static const int delay_in_seconds = 4;
 //----------------------------------------
-#define LEN_MSG 160
-static char buf_msg[ LEN_MSG + 1 ] = "Hello from module!\n";
+//#define LEN_MSG 160
+//static char buf_msg[ LEN_MSG + 1 ] = "Hello from module!\n";
+void my_timer_function(unsigned long data)
+{
+	int new_delay = delay_in_seconds*HZ;   
+	if (++num_of_prints < prints_count)
+		mod_timer(&my_timer, jiffies + new_delay);  /* new expiration */
+	else
+		printk(KERN_INFO "======== done timers. kernel_hello_irq module ===========\n");
+
+	printk(KERN_INFO "\n" "Hello from linux" ": ===== current: num_of_prints = %d\n", num_of_prints);
+}
 
 static int __init kernel_hello_irq_init(void)
 {
 	int ret = 0;
-	printk(KERN_INFO "======== kernel_hello_irq module installed ,===========\n");
-err:
+	int delay;
+	printk(KERN_INFO "======== kernel_hello_irq module installed. ===========\n");
+	printk(KERN_INFO "prints_count : %d\n", prints_count);
+	init_timer(&my_timer);
+	delay = delay_in_seconds*HZ;         /* 4 seconds from now */
+	my_timer.expires = jiffies + delay;    /* timer expires in delay ticks */
+	my_timer.data = 0;                     /* zero is passed to the timer handler */
+	my_timer.function = my_timer_function;       /* function to run when timer expires */
+	add_timer(&my_timer);
+
+//err:
 	return ret;// Non-zero return means that the module couldn't be loaded.
 
 }
@@ -35,7 +59,6 @@ err:
 static void __exit kernel_hello_irq_cleanup(void)
 {
 
-	sysfs_cleanup();
 	printk(KERN_INFO "========= kernel_hello_irq module removed ==================\n");
 }
 
