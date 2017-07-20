0bbef19ad48635957827db4caaaa57af08935778
Ultrafeel <miketut@gmail.com>
2017-07-20 17:44:00 +0300

    extra subfolder

--- S11load_kernel_hello_irq.sh
+++ S11load_kernel_hello_irq.sh
@@ -3,5 +3,5 @@ echo ====my load script arg=$1========
 if [ "$1" = "start" ]
 then
 	echo "start"
-	insmod /lib/modules/`uname -r`/kernel_hello_irq.ko prints_count=5
+	insmod /lib/modules/`uname -r`/extra/kernel_hello_irq.ko prints_count=5
 fi
