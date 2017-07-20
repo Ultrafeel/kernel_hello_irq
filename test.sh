05dc1867828588eb5f1ded1c97613a5b78839b99
Ultrafeel <miketut@gmail.com>
2017-07-20 17:51:03 +0300

    fix test.sh

--- test.sh
+++ test.sh
@@ -1,18 +1,3 @@
 #!/bin/sh
 
-arg_class=kerncalc_arg-class
-b=_________________
-
-fail="$b fail"
-right="$b success"
-echo '+' op 
-echo 12 > /sys/class/$arg_class/argument1
-echo 23 > /sys/class/$arg_class/argument2
-echo + > /dev/operand
-res=`cat /dev/result`
-if [ "$res" -eq 35 ]
-then
-	echo $right
-else
-	echo $fail
-fi
+/etc/init.d/S11load_kernel_hello_irq.sh start
